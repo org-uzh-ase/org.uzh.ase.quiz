d630fe981950073b561281dfc431d9aa76d28080
ltoedt <laura.toedtli@uzh.ch>
2020-03-07 20:11:43 +0100

    modified API to return JSON

---
+++ microservice.example/src/main/java/org/uzh/ase/microservice/example/model/Example.java
@@ -0,0 +1,13 @@
+package org.uzh.ase.microservice.example.model;
+
+public class Example {
+    private final String text;
+
+    public Example(){
+        this.text = "Hello World!";
+    }
+
+    public String getText() {
+        return text;
+    }
+}
--- microservice.example/src/main/java/org/uzh/ase/microservice/example/restservice/ExampleController.java
+++ microservice.example/src/main/java/org/uzh/ase/microservice/example/restservice/ExampleController.java
@@ -1,14 +1,16 @@
 package org.uzh.ase.microservice.example.restservice;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.uzh.ase.microservice.example.model.Example;
 
 @RestController
 public class ExampleController {
 
     @RequestMapping("/example")
-    public String example(){
-        return "Hello World!";
+    public Example example(){
+        return new Example();
     }
 }
