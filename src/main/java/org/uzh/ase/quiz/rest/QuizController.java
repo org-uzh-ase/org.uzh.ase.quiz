4201843eb243b66792ae538653292d490a364ff7
ltoedt <laura.toedtli@uzh.ch>
2020-04-18 21:49:49 +0200

    and again sonar issues fixed

--- src/main/java/org/uzh/ase/quiz/rest/QuizController.java
+++ src/main/java/org/uzh/ase/quiz/rest/QuizController.java
@@ -1,14 +1,11 @@
 package org.uzh.ase.quiz.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.uzh.ase.quiz.model.Quiz;
 
 @RestController
 public class QuizController {
-        @RequestMapping(path = "/api/quizzes/quiz", method = RequestMethod.GET)
+        @GetMapping(path = "/api/quizzes/quiz")
         public Quiz getQuiz(@RequestParam(value = "level", defaultValue="1") String level){
             return new Quiz();
         }
