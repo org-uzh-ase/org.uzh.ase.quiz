82501873ef266507200fda892a1d608c1efcd1f6
ltoedt <laura.toedtli@uzh.ch>
2020-04-18 21:03:53 +0200

    add quiz controller, remove example

--- src/main/java/org/uzh/ase/Application.java
+++ src/main/java/org/uzh/ase/Application.java
@@ -1,17 +1,13 @@
 package org.uzh.ase;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.uzh.ase.quiz.model.Quiz;
-import org.uzh.ase.quiz.repository.QuizRepository;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
@@ -22,17 +18,9 @@ public class Application implements CommandLineRunner {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/example").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/api/quizzes/quiz").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
 
-	@Autowired
-	private QuizRepository repository;
-
-	@Override
-	public void run(String... args) throws Exception {
-		repository.deleteAll();
-		repository.save(new Quiz("imdb_id_example"));
-	}
 }
--- src/main/java/org/uzh/ase/quiz/model/Quiz.java
+++ src/main/java/org/uzh/ase/quiz/model/Quiz.java
@@ -4,32 +4,45 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("quiz")
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
 public class Quiz {
-    @Id
-    private String id;
+    private String description;
+    private List<Movie> answers;
+    private String correctAnswer;
 
-    @Indexed
-    private String imdbId;
-
-    public String getId() {
-        return id;
+    public String getDescription() {
+        return description;
     }
 
-    public String getImdbId() {
-        return imdbId;
+    public List<Movie> getAnswers() {
+        return answers;
     }
 
-public Quiz(){}
-
-public Quiz(String imdbId){
-        this.imdbId = imdbId;
+    public String getCorrectAnswer() {
+        return correctAnswer;
     }
 
-    @Override
-    public String toString() {
-        return String.format(
-                "Quiz[id=%s, imdb_id='%s']",
-                id, imdbId);
+    public Quiz(){
+    this.description = "Woody, a good-hearted cowboy doll who belongs to a young boy named Andy, sees his position as Andy's favorite toy jeopardized when his mom buys him a Buzz Lightyear action figure. Even worse, the arrogant Buzz thinks he's a real spaceman on a mission to return to his home planet. When Andy's family moves to a new house, Woody and Buzz must escape the clutches of maladjusted neighbor Sid Phillips and reunite with their boy.";
+    List<Movie> answers = new ArrayList<>();
+    answers.add(new Movie("114709", "https://upload.wikimedia.org/wikipedia/commons/0/0a/Toy_Story_logo.svg", "Toy Story", "SAND1"));
+    answers.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/f/f0/Toy_Story_2.svg", "Toy Story 2", "SAND2"));
+    answers.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/6/6a/Toy_Story_3.svg", "Toy Story 3", "SAND3"));
+    answers.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/9/93/Toy_Story_4.jpeg", "Toy Story 4", "SAND4"));
+    this.answers = answers;
+    this.correctAnswer = "SAND";
+}
+
+public Quiz(String description, List<Movie> answers, String correctAnswer){
+    this.description = description;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
     }
 }
