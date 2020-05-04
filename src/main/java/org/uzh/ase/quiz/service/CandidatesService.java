66b45401a43302755a87bacabad629ff6366fb5d
ltoedt <laura.toedtli@uzh.ch>
2020-05-04 14:05:03 +0200

    Revert "use config.properties"
    This reverts commit 8ae012f97b93aa587f264a6d21dc9dd8f89d4df4.

--- src/main/java/org/uzh/ase/quiz/service/CandidatesService.java
+++ src/main/java/org/uzh/ase/quiz/service/CandidatesService.java
@@ -6,44 +6,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.uzh.ase.quiz.model.Movie;
 
-import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 @Service
 public class CandidatesService {
-    private String baseUrl;
-
-    public CandidatesService(){
-        baseUrl = "http://" + getBaseurl() + ":8082/";
-    }
+    static final String BASEURL = "http://localhost:8082/";
 
     @Autowired
     RestTemplate restTemplate;
 
-    private String getBaseurl(){
-        Properties properties = new Properties();
-
-        try {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
-
-            if (inputStream != null) {
-                properties.load(inputStream);
-            } else {
-                return "localhost";
-            }
-
-            return properties.get("basePath").toString();
-        }
-        catch (Exception e){
-            return "localhost";
-        }
-    }
-
     public List<Movie> getCandidates(String movieId, int level) {
-        Movie[] response = restTemplate.getForObject(baseUrl + "api/candidates?movie_id=" + movieId + "&level=" + level, Movie[].class);
+        Movie[] response = restTemplate.getForObject(BASEURL + "api/candidates?movie_id=" + movieId + "&level=" + level, Movie[].class);
 
         List<Movie> result = new ArrayList<>();
 
