0f877bf6c7d66e88b129445e1d68b16ac5294c03
Vladimir Masarik <masarik.vladimir7@gmail.com>
2020-05-09 15:17:36 +0200

    updated run.sh to always upload DB

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-mongod --bind_ip_all --port 27021
+mongod --bind_ip_all --port 27021 &
 
 # CODE in case of debugging need.
-# mongorestore --port 27021 -d movies movies
+mongorestore --port 27021 -d movies movies 2> /dev/null
+
+while true; do sleep 100; done
 # kill %1
 # sleep 20 # need to wait for the process to die
 # mongod --bind_ip_all --port 27021
