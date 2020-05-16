430a91c5ea4e996924e1bdcaf05afb74b1d5e146
ltoedt <laura.toedtli@uzh.ch>
2020-05-16 13:31:23 +0200

    remove unused constructor

--- src/main/java/org/uzh/ase/quiz/model/Quiz.java
+++ src/main/java/org/uzh/ase/quiz/model/Quiz.java
@@ -1,6 +1,5 @@
 package org.uzh.ase.quiz.model;
 
-import java.util.ArrayList;
 import java.util.List;
 /**
  * Data Transfer Object between Frontend and Quiz Microservice.
@@ -21,17 +20,6 @@ public class Quiz {
     public String getCorrectAnswer() {
         return correctAnswer;
     }
-
-    public Quiz(){
-    this.description = "Woody, a good-hearted cowboy doll who belongs to a young boy named Andy, sees his position as Andy's favorite toy jeopardized when his mom buys him a Buzz Lightyear action figure. Even worse, the arrogant Buzz thinks he's a real spaceman on a mission to return to his home planet. When Andy's family moves to a new house, Woody and Buzz must escape the clutches of maladjusted neighbor Sid Phillips and reunite with their boy.";
-    List<Movie> ans = new ArrayList<>();
-    ans.add(new Movie("114709", "https://upload.wikimedia.org/wikipedia/commons/0/0a/Toy_Story_logo.svg", "Toy Story", "SAND1"));
-    ans.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/f/f0/Toy_Story_2.svg", "Toy Story 2", "SAND2"));
-    ans.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/6/6a/Toy_Story_3.svg", "Toy Story 3", "SAND3"));
-    ans.add(new Movie("", "https://upload.wikimedia.org/wikipedia/de/9/93/Toy_Story_4.jpeg", "Toy Story 4", "SAND4"));
-    this.answers = ans;
-    this.correctAnswer = "SAND1";
-}
 
     /**
      * Quiz constructor
